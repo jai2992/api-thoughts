194dd16dd2f1adbb0b14febce5f113ba301d5777
JAIGURU <136327019+jai2992@users.noreply.github.com>
2025-07-09 06:21:50 +0000

    added export and basepath in next-config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export', // Enables static HTML export
+    basePath: '/https://github.com/jai2992/api-thoughts', // Replace with your repository name (e.g., '/my-project')
 };
 
 export default nextConfig;
